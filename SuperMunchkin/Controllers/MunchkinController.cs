ef0a090c11567b0847a0c14ada1a48751554c45e
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2019-03-29 13:48:44 +0100

    Controllers & Navigation

---
+++ SuperMunchkin/Controllers/MunchkinController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMunchkin.Controllers
+{
+    public class MunchkinController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

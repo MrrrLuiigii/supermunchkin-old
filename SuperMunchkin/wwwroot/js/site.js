4d196c572c3de9b7912a5a98d960eef7d5fbd39a
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2019-03-29 12:01:21 +0100

    Navbar & Bootstrap 4

---
+++ SuperMunchkin/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+$(document).ready(function () {
+    var fixHeight = function () {
+        $(".navbar-nav").css("max-height", document.documentElement.clientHeight - 150);
+    }
+    fixHeight();
+    $(window).resize(function () {
+        fixHeight();
+    });
+    $(".navbar .navbar-toggler").on("click", function () {
+        fixHeight();
+    });
+    $(".navbar-toggler, .overlay").on("click", function () {
+        $(".mobileMenu, .overlay").toggleClass("open");
+    });
+});

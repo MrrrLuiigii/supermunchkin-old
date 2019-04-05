810f936113dd16147eb101991448252e8ee4534c
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-04-05 14:03:51 +0200

    Revert "Revert 2.0"
    This reverts commit 894d473d525dc856e04dc6999e2f5d0bd41ac39b.

--- DAL/Repositories/GameRepository.cs
+++ DAL/Repositories/GameRepository.cs
@@ -1,54 +1,10 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-=======
-﻿using System.Collections.Generic;
-using DAL.Interfaces;
-using Models.Enums;
-using Models.Interfaces;
->>>>>>> parent of b64d1f3... Removing Model Interfaces
 
 namespace DAL.Repositories
 {
     class GameRepository
     {
-<<<<<<< HEAD
-=======
-        public void AddGame(IGame game)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void AddMunchkin(IMunchkin munchkin)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void AdjustGameStatus(GameStatus status)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<IGame> GetAllGames()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IGame GetGameById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void RemoveMunchkin(IMunchkin munchkin)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetWinner(IMunchkin munchkin)
-        {
-            throw new System.NotImplementedException();
-        }
->>>>>>> parent of b64d1f3... Removing Model Interfaces
     }
 }
--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -1,63 +1,10 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-=======
-﻿using System.Collections.Generic;
-using DAL.Interfaces.User;
-using Models.Interfaces;
->>>>>>> parent of b64d1f3... Removing Model Interfaces
 
 namespace DAL.Repositories
 {
     class UserRepository
     {
-<<<<<<< HEAD
-=======
-        public void AddMunchkin(IMunchkin munchkin)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void AddUser(IUser user)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<IMunchkin> GetAllMunchkins()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<IUser> GetAllUsers()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IMunchkin GetMunchkinById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IUser GetUserById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IUser Login(string username, string password)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Logout()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void RemoveMunchkin(IMunchkin munchkin)
-        {
-            throw new System.NotImplementedException();
-        }
->>>>>>> parent of b64d1f3... Removing Model Interfaces
     }
 }

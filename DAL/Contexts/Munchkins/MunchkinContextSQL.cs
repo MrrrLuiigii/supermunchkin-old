cbeaae86c4c6238edcf68f954aca860124aed03d
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-04-23 14:29:17 +0200

    Implemented munchkinSQL

--- DAL/Contexts/Munchkins/MunchkinContextSQL.cs
+++ DAL/Contexts/Munchkins/MunchkinContextSQL.cs
@@ -1,10 +1,31 @@
 ﻿using DAL.Interfaces.Munchkins;
+using Databases;
+using Models;
 using Models.Enums;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 
 namespace DAL.Contexts.Munchkins
 {
-    public class MunchkinContextSQL
+    public class MunchkinContextSQL : IMunchkinContext
     {
-        
+        private Database database = new Database();
+
+        public void AdjustMunchkin(Munchkin munchkin)
+        {
+            string sql = $"update `munchkin` set `Gender` = @Gender, `Level` = @Level, `Gear` = @Gear where `MunchkinId` = @MunchkinId";
+
+            List<MySqlParameter> parameters = new List<MySqlParameter>();
+            parameters.Add(new MySqlParameter("@Gender", munchkin.Gender));
+            parameters.Add(new MySqlParameter("@Level", munchkin.Level));
+            parameters.Add(new MySqlParameter("@Gear", munchkin.Gear));
+            parameters.Add(new MySqlParameter("@MunchkinId", munchkin.Id));
+
+            if (database.ExecuteQueryWithStatus(sql, parameters) != ExecuteQueryStatus.OK)
+            {
+                throw new Exception("Something went wrong. Sorry for the inconvenience.");
+            }
+        }
     }
 }

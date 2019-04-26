289aab5c0ab8b29c7e5365374cca6972d97cbcb5
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-04-26 14:31:54 +0200

    Added Logic.Test project

---
+++ Logic.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ChangeLevelTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ChangeLevelTooHighTest()
+        {
+        }
+    }
+}

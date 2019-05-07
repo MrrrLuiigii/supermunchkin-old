d4f9581d0d102d8d4014119710666ba2f2178697
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-07 14:15:40 +0200

    Added UserLogicTest

---
+++ Logic.Test/Users/UserLogicTest.cs
@@ -0,0 +1,26 @@
+using Factories;
+using Logic.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Models.Enums;
+
+namespace Logic.Test.Users
+{
+    [TestClass]
+    public class UserLogicTest
+    {
+        UserLogic userLogic;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CreateMunchkinTest()
+        {
+
+        }
+    }
+}

c2c0e059eaef5d2b64c3b07768ee0d924d9cc465
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-10 09:45:09 +0200

    Added UserCollectionLogicTest

---
+++ Logic.Test/Users/UserCollectionLogicTest.cs
@@ -0,0 +1,27 @@
+﻿using Factories;
+using Logic.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Models.Enums;
+
+namespace Logic.Test.Users
+{
+    [TestClass]
+    public class UserCollectionLogicTest
+    {
+        UserCollectionLogic userCollectionLogic;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            userCollectionLogic = new UserCollectionLogic();
+        }
+
+        [TestMethod]
+        public void AddUserTest()
+        {
+            User user = new User("NewUser", "password", "new@email.com");
+            userCollectionLogic.AddUser(user);
+        }
+    }
+}

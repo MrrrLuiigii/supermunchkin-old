593c9416d3800742f0c9f8d751e557d27d0d330e
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-10 13:30:52 +0200

    Added mailhandler

---
+++ Logic/MailHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class MailHandler
+    {
+        public static void SendMail(string receiver, string subject, string body)
+        {
+
+        }
+    }
+}

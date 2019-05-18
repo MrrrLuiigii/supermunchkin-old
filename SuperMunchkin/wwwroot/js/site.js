db4c0430b9219485e8963dfd61f34468910e712d
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-18 15:25:34 +0200

    Added javascript for munchkins in game
    also datetime is visible

--- SuperMunchkin/wwwroot/js/site.js
+++ SuperMunchkin/wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
+// Navigation bar
 $(document).ready(function () {
     var fixHeight = function () {
         $(".navbar-nav").css("max-height", document.documentElement.clientHeight - 150);
@@ -16,3 +18,14 @@ $(document).ready(function () {
         $(".mobileMenu, .overlay").toggleClass("open");
     });
 });
+
+// Popup with munchkin names
+function showPopup(gameId) {
+    var popup = document.getElementById(gameId);
+    popup.classList.replace("d-none", "d-inline-block");
+    popup.style.marginLeft = -(popup.offsetWidth / 2) + 'px';
+    popup.style.marginTop = -(popup.offsetHeight / 2) + 'px';
+}
+function hidePopup(gameId) {
+    document.getElementById(gameId).classList.replace("d-inline-block", "d-none");
+}

08cc97590f6655f5cedd9248d665fce71781332f
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-18 21:36:28 +0200

    Added monster model

---
+++ Models/Monster.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class Monster
+    {
+        public int Level { get; set; }
+        public int Modifier { get; set; }
+
+        public Monster()
+        {
+            Level = 0;
+            Modifier = 0;
+        }
+    }
+}

b468e4912304b1f2ebb2fab283497062c2d7045f
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-18 21:57:54 +0200

    Added battle controller and index view

---
+++ SuperMunchkin/Controllers/BattleController.cs
@@ -0,0 +1,31 @@
+﻿using Logic.Games;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+
+namespace SuperMunchkin.Controllers
+{
+    public class BattleController : Controller
+    {
+        private GameCollectionLogic gameCollectionLogic = new GameCollectionLogic();
+
+        public IActionResult Index()
+        {
+            int gameId = Convert.ToInt32(Request.Cookies["GameId"]);
+            Game game = gameCollectionLogic.GetGameById(gameId);
+
+
+            return View();
+        }
+
+        public IActionResult AddMunchkin()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult AddMonster()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}

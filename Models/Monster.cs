d30c53ce4e8e0dfeb2039c389ca093f698967811
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-18 21:57:38 +0200

    Added monster model

--- Models/Monster.cs
+++ Models/Monster.cs
@@ -2,11 +2,14 @@
 {
     public class Monster
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
         public int Level { get; set; }
         public int Modifier { get; set; }
 
-        public Monster()
+        public Monster(string name)
         {
+            Name = name;
             Level = 0;
             Modifier = 0;
         }

fd0efb296fe8fafdb13bcf5ce6e2212d698bd3f4
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-21 10:01:01 +0200

    Added Battle

---
+++ Models/Battle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Models.Enums;
+
+namespace Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public BattleStatus Status { get; set; }
+        public DateTime DateTimeBattled { get; set; }
+        public List<Munchkin> Munchkins { get; set; }
+        public List<Monster> Monsters { get; set; }
+
+        public Battle()
+        {
+            Status = BattleStatus.Ongoing;
+            DateTimeBattled = DateTime.Now;
+            Munchkins = new List<Munchkin>();
+            Monsters = new List<Monster>();
+        }
+
+        public Battle(int id, BattleStatus status, DateTime dateTimeBattled, List<Munchkin> munchkins, List<Monster> monsters)
+        {
+            Id = id;
+            Status = status;
+            DateTimeBattled = dateTimeBattled;
+            Munchkins = munchkins;
+            Monsters = monsters;
+        }
+    }
+}

9547be5e43e7109e8c41c95e427a8afafb369a32
nickyjjanse <43231781+MrrrLuiigii@users.noreply.github.com>
2019-05-29 10:20:21 +0200

    Added integrationtest project

---
+++ Logic.IntegrationTest/Munchkins/MunchkinLogicIntegrationTest.cs
@@ -0,0 +1,89 @@
+using Factories;
+using Logic.Munchkins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Models.Enums;
+
+namespace Logic.Test.Munchkins
+{
+    [TestClass]
+    public class MunchkinLogicIntegrationTest
+    {
+        MunchkinLogic munchkinLogic;
+        Munchkin munchkin;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            munchkin = new Munchkin(1, "MrrrLuiigii", MunchkinGender.Male, 5, 5);
+            munchkinLogic = new MunchkinLogic(MunchkinFactory.GetMunchkinRepositoryTest());
+        }
+
+        [TestMethod]
+        public void AdjustLevelUpTest()
+        {
+            munchkinLogic.AdjustLevel(munchkin, AdjustStats.Up);
+            Assert.AreEqual(6, munchkin.Level);
+        }
+
+        [TestMethod]
+        public void AdjustLevelDownTest()
+        {
+            munchkinLogic.AdjustLevel(munchkin, AdjustStats.Down);
+            Assert.AreEqual(4, munchkin.Level);
+        }
+
+        [TestMethod]
+        public void AdjustLevelTooHighTest()
+        {
+            munchkin.Level = 10;
+            munchkinLogic.AdjustLevel(munchkin, AdjustStats.Up);
+            Assert.AreEqual(10, munchkin.Level);
+        }
+
+        [TestMethod]
+        public void AdjustLevelTooLowTest()
+        {
+            munchkin.Level = 0;
+            munchkinLogic.AdjustLevel(munchkin, AdjustStats.Down);
+            Assert.AreEqual(0, munchkin.Level);
+        }
+
+        [TestMethod]
+        public void AdjustGearUpTest()
+        {
+            munchkinLogic.AdjustGear(munchkin, AdjustStats.Up);
+            Assert.AreEqual(6, munchkin.Gear);
+        }
+
+        [TestMethod]
+        public void AdjustGearDownTest()
+        {
+            munchkinLogic.AdjustGear(munchkin, AdjustStats.Down);
+            Assert.AreEqual(4, munchkin.Gear);
+        }
+
+        [TestMethod]
+        public void AdjustGearTooLowTest()
+        {
+            munchkin.Gear = 0;
+            munchkinLogic.AdjustGear(munchkin, AdjustStats.Down);
+            Assert.AreEqual(0, munchkin.Gear);
+        }
+
+        [TestMethod]
+        public void AdjustGenderMaleToFemaleTest()
+        {
+            munchkinLogic.AdjustGender(munchkin);
+            Assert.AreEqual(MunchkinGender.Female, munchkin.Gender);
+        }
+
+        [TestMethod]
+        public void AdjustGenderFemaleToMaleTest()
+        {
+            munchkin.Gender = MunchkinGender.Female;
+            munchkinLogic.AdjustGender(munchkin);
+            Assert.AreEqual(MunchkinGender.Male,  munchkin.Gender);
+        }
+    }
+}
---
+++ Logic.IntegrationTest/Users/UserCollectionLogicIntegrationTest.cs
@@ -0,0 +1,69 @@
+﻿using Factories;
+using Logic.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace Logic.Test.Users
+{
+    [TestClass]
+    public class UserCollectionLogicIntegrationTest
+    {
+        UserCollectionLogic userCollectionLogic;
+        User user;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            userCollectionLogic = new UserCollectionLogic(UserFactory.GetUserCollectionRepositoryTest());
+            user = new User(1, "Nicky", "admin", "nicky@gmail.com");
+        }
+
+        [TestMethod]
+        public void AddUserTest()
+        {
+            user.Username = "NewUser";
+            user.Email = "newuser@gmail.com";
+            Assert.IsTrue(userCollectionLogic.AddUser(user));
+        }
+
+        [TestMethod]
+        public void AddUserUsernameTakenTest()
+        {
+            user.Email = "newemail@gmail.com";
+            Assert.IsFalse(userCollectionLogic.AddUser(user));
+        }
+
+        [TestMethod]
+        public void AddUserEmailTakenTest()
+        {
+            user.Username = "NewUser";
+            Assert.IsFalse(userCollectionLogic.AddUser(user));
+        }
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            Assert.AreEqual(user.Id, userCollectionLogic.Login(user.Username, user.Password).Id);
+        }
+
+        [TestMethod]
+        public void LoginWrongUsernameTest()
+        {
+            user.Username = "Nickyy";
+            Assert.IsNull(userCollectionLogic.Login(user.Username, user.Password));
+        }
+
+        [TestMethod]
+        public void LoginWrongPasswordTest()
+        {
+            user.Username = "Nicky";
+            user.Password = HashPassword("wrong");
+            Assert.IsNull(userCollectionLogic.Login(user.Username, user.Password));
+        }
+
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+        }
+    }
+}
